7009e6e3cb2f1e71b52f262eb94f4b0c778210d8
juliswer <julianswer36@gmail.com>
2022-09-26 20:19:27 -0300

    uploading list tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createTask } from "./graphql/mutations";
+import { listTasks } from "./graphql/queries";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState({
+    name: "",
+    description: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await API.graphql(
+        graphqlOperation(createTask, { input: task })
+      );
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const result = await API.graphql(graphqlOperation(listTasks));
+      setTasks(result.data.listTasks.items);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <textarea
+          name="description"
+          placeholder="Description"
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+        ></textarea>
+        <button>Submit</button>
+      </form>
+      {tasks.map((task) => {
+        return (
+          <div key={task.id}>
+            <h2>{task.name}</h2>
+            <p>{task.description}</p>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
